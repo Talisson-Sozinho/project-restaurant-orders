7c8e222921d8215a35b34fb40f920d0f68880112
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-29 01:21:47 -0300

    requisito 03 - feito a classe "MenuData"

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,27 @@
-# Req 3
+import pandas as pd
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        pd_data_frame = pd.read_csv(source_path)
+
+        self.dishes = set()
+
+        dict_of_dishes = {}
+
+        for dish, price, ingredient, recipe_amount in pd_data_frame.itertuples(
+            index=False
+        ):
+            if dish not in dict_of_dishes:
+                dict_of_dishes[dish] = Dish(dish, price)
+
+            ingredient_instance = Ingredient(ingredient)
+
+            dict_of_dishes[dish].add_ingredient_dependency(
+                ingredient_instance, recipe_amount
+            )
+
+        for dish in dict_of_dishes.values():
+            self.dishes.add(dish)
