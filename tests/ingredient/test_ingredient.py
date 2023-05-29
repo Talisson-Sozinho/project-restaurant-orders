04314a1a6474abbbd4ddde016d6985bb7599c666
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-28 23:44:29 -0300

    requisito 01 - teste para classe "Ingredient"

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('bacon')
+    ingredient_2 = Ingredient('bacon')
+    ingredient_3 = Ingredient('manteiga')
+
+    assert ingredient.restrictions == {
+        Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED
+    }
+    assert ingredient == ingredient_2
+    assert hash(ingredient) == ingredient.__hash__()
+    assert hash(ingredient) != hash(ingredient_3)
+    assert ingredient.name == 'bacon'
+    assert repr(ingredient) == "Ingredient('bacon')"
